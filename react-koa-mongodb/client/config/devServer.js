3406f68f4c4c9a02693a4a701debfc3489b3960d
PC-hp-Keun\Administrator <746542397@qq.com>
2018-04-21 01:20:26 +0800

    feat：client/config文件配置

--- react-koa-mongodb/client/config/devServer.js
+++ react-koa-mongodb/client/config/devServer.js
@@ -2,6 +2,12 @@
 const webpack = require('webpack');
 // 启动一个服务器
 const WebpackDevServer = require('webapck-dev-server');
+/**
+ * 使用nodejs完成一个简单的本地服务器, 并支持热替换功能,
+ * 主要是检测webpack打包过程和让程序支持热加载, 但是应用了这个插件并不会完成所有热加载效果,
+ * 比如我们在使用redux时, 就会出问题, 因为这个热替换并不能保留state(状态),
+ * 所以使用时, 每次保存, react组件的状态就不会保留, 所以需要引入另一个插件react-hot-loader来解决这个问题,
+ */
 
 // 加载webpack配置文件
 const config = require('./webpack.config.dev');
